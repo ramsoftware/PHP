6cb63c8599616705723fd75b78fda5aa19479559
Rafael Alberto Moreno Parra <ramsoftware@gmail.com>
2026-01-10 16:49:30 -0500

    Add files via upload

---
+++ 05/Ej02.php
@@ -0,0 +1,7 @@
+<?php
+//Concatenar cadenas y valores numéricos
+$cadena1 = 3;
+$cadena2 = "abcde";
+$cadena3 = 5;
+$cadena4 = $cadena1 . $cadena2 . $cadena3 . "efg";
+echo $cadena4;
---
+++ 05/Ej05.php
@@ -0,0 +1,8 @@
+<?php
+//Convertir a mayúsculas y minúsculas
+$cadena = "ÉsTa Es UNa PrUEBa dE CAmbio de MAyúsCULas y MINúscULas";
+$mayuscula = strtoupper($cadena); //Convierte a mayúsculas
+$minuscula = strtolower($cadena); //Convierte a minúsculas
+echo $mayuscula;
+echo "<br>";
+echo $minuscula;
